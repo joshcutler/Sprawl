905a827d80121f05bc341a4e7f07dffb0fc688ca
joshcutler <josh.cutler@gmail.com>
2012-10-24 21:33:23 -0400

    Switched to fullscreen

--- src/sprawl/Constants.java
+++ src/sprawl/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 	
 	public static final float GAME_SPEED = 0.1f; //In RL seconds per game minutes
 	
-	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 640;
+	public static final int WINDOW_WIDTH = 1600;
+	public static final int WINDOW_HEIGHT = 1200;
 	
 	public static final int BLOCK_SIZE = 10;
 	public static final int WORLD_HEIGHT = 1028;
--- src/sprawl/RenderingEngine.java
+++ src/sprawl/RenderingEngine.java
@@ -26,8 +26,8 @@ public class RenderingEngine {
 	
 	public static void initOpenGL() {
 		try {
-			Display.setDisplayMode(new DisplayMode(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
-            Display.setTitle(Constants.APPLICATION_NAME);
+			RenderingEngine.setDisplayMode(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT, true);
+			Display.setTitle(Constants.APPLICATION_NAME);
             Display.create();
             System.out.println("OpenGL Version: " + GL11.glGetString(GL11.GL_VERSION));
 		} catch (LWJGLException e) {
@@ -48,6 +48,67 @@ public class RenderingEngine {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
         glViewport(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+	}
+	
+	/**
+	 * Set the display mode to be used 
+	 * 
+	 * @param width The width of the display required
+	 * @param height The height of the display required
+	 * @param fullscreen True if we want fullscreen mode
+	 */
+	public static void setDisplayMode(int width, int height, boolean fullscreen) {
+
+	    // return if requested DisplayMode is already set
+	    if ((Display.getDisplayMode().getWidth() == width) && 
+	        (Display.getDisplayMode().getHeight() == height) && 
+		(Display.isFullscreen() == fullscreen)) {
+		    return;
+	    }
+
+	    try {
+	        DisplayMode targetDisplayMode = null;
+			
+		if (fullscreen) {
+		    DisplayMode[] modes = Display.getAvailableDisplayModes();
+		    int freq = 0;
+					
+		    for (int i=0;i<modes.length;i++) {
+		        DisplayMode current = modes[i];
+						
+			if ((current.getWidth() == width) && (current.getHeight() == height)) {
+			    if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+			        if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+				    targetDisplayMode = current;
+				    freq = targetDisplayMode.getFrequency();
+	                        }
+	                    }
+
+			    // if we've found a match for bpp and frequence against the 
+			    // original display mode then it's probably best to go for this one
+			    // since it's most likely compatible with the monitor
+			    if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+	                        (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+	                            targetDisplayMode = current;
+	                            break;
+	                    }
+	                }
+	            }
+	        } else {
+	            targetDisplayMode = new DisplayMode(width,height);
+	        }
+
+	        if (targetDisplayMode == null) {
+	            System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+	            return;
+	        }
+
+	        Display.setDisplayMode(targetDisplayMode);
+	        Display.setFullscreen(fullscreen);
+				
+	    } catch (LWJGLException e) {
+	        System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+	    }
 	}
 	
 	public static void initTextures() {
